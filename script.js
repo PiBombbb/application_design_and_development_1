b510bfd3e8d00e7db20764d23f1b903d00fc6fb1
PiBombbb <pi00jirapat@gmail.com>
2026-06-22 21:06:48 +0700

    added Nasa for testing

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const modal = document.getElementById('simon_anim');
 const openButton = document.getElementById('simon_open');
 const closeButton = document.getElementById('simon_close');
+const fetchNasaBtn = document.getElementById('fetchNasaBtn');
+const nasa_display = document.getElementById('nasa_display');
+
 
 // 2. Tell the open button to listen for a click and open the modal
 openButton.addEventListener('click', () => {
@@ -11,4 +14,15 @@ openButton.addEventListener('click', () => {
 // 3. Tell the close button to listen for a click and close the modal
 closeButton.addEventListener('click', () => {
     modal.close();
+});
+fetchNasaBtn.addEventListener('click', () => {
+    const minLat = document.getElementById('minLat').value;
+    const minLon = document.getElementById('minLon').value;
+    const maxLat = document.getElementById('maxLat').value;
+    const maxLon = document.getElementById('maxLon').value;
+
+    const finalImageUrl = `https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?BBOX=${minLat},${minLon},${maxLat},${maxLon}&REQUEST=GetMap&FORMAT=image/jpeg&VERSION=1.3.0&LAYERS=BlueMarble_ShadedRelief_Bathymetry&WIDTH=600&HEIGHT=300&CRS=EPSG:4326`;
+
+    nasa_display.src = final_image_url;
+    nasa_display.style.display = "block";
 });
