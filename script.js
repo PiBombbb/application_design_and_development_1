fcb3f83c9bcc1213cd357ce3774c3933b7470e62
PiBombbb <112552729+PiBombbb@users.noreply.github.com>
2026-06-24 11:20:28 +0700

    Update script.js - removed NASA display

--- script.js
+++ script.js
@@ -2,31 +2,10 @@
 const modal = document.getElementById('simon_anim');
 const open_button = document.getElementById('simon_open');
 const close_button = document.getElementById('simon_close');
-const fetch_nasa = document.getElementById('fetch_nasa');
-const nasa_display = document.getElementById('nasa_display');
-const debug_link = document.getElementById('debug_link');
-
-
 open_button.addEventListener('click', () => {
     modal.showModal();
 });
 
 close_button.addEventListener('click', () => {
     modal.close();
-});
-fetch_nasa.addEventListener('click', () => {
-    const minLat = document.getElementById('minLat').value;
-    const minLon = document.getElementById('minLon').value;
-    const maxLat = document.getElementById('maxLat').value;
-    const maxLon = document.getElementById('maxLon').value;
-    const latDiff = maxLat - minLat;
-    const lonDiff = maxLon - minLon;
-    const imageHeight = 400;
-    const imageWidth = Math.round(imageHeight * (lonDiff / latDiff));
-    const final_image_url = `https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?BBOX=${minLat},${minLon},${maxLat},${maxLon}&REQUEST=GetMap&FORMAT=image/jpeg&VERSION=1.3.0&LAYERS=BlueMarble_ShadedRelief_Bathymetry&WIDTH=${imageWidth}&HEIGHT=${imageHeight}&CRS=EPSG:4326`;
-    nasa_display.src = final_image_url;
-    nasa_display.style.display = "block";
-    debug_link.href = final_image_url;
-    debug_link.textContent = `URL to Image: ${final_image_url}`;
-    debug_link.style.display = "inline-block";
 });
