9edf7b5d4463ccc90b60762c6f0aee59be5c3422
PiBombbb <pi00jirapat@gmail.com>
2026-09-09 06:51:36 +0700

    Added some Supabase code

--- university/script.js
+++ university/script.js
@@ -3,10 +3,20 @@ const root = document.documentElement;
 const USERNAME_KEY = 'username';
 const usernameForm = document.getElementById('username-form');
 const usernameInput = document.getElementById('username-input');
+const userGreeting = document.getElementById('user-greeting');
 const changeUsernameBtn = document.getElementById('change-username');
+
 const saved = localStorage.getItem('theme');
 const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches; //Does system prefer dark?
 const startDark = saved ? saved === 'dark' : systemPrefersDark; //Condition for dark mode
+
+const mainTable = document.getElementById('main-table');
+
+const SUPABASE_URL = "https://jooisbhkdnqtbsbxrhet.supabase.co";
+const SUPABASE_KEY = "sb_publishable_y9goeAs-PsNrPzM0Y1KcNA_Hr3O1sm2";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+let rows = [];
 
 function applyTheme(isDark) {
     root.classList.toggle('dark', isDark);
@@ -38,6 +48,33 @@ function renderUserState() {
     }
 }
 
+function renderTable() {
+    mainTable.innerHTML = '';
+}
+
+async function testConnection() {
+    const { data, error } = await supabaseClient.from(
+        'University Rows'
+    ).select('id').limit(1)
+    if (error) {
+        console.error('Connection failed:', error.message)
+    } else {
+        console.log('Connected successfully! Data:', data)
+    }
+}
+
+async function fetchData() {
+    const { data: rowData, error: rowError } = await supabaseClient
+        .from('University Rows')
+        .select('*');
+
+    if (!rowError && rowData) {
+        rows = rowData;
+    } else {
+        console.error("Failed to load min table:", rowError);
+    }
+    //add render functions here to render after
+}
 
 toggleBtn.addEventListener('click', () => {
     const isDark = !root.classList.contains('dark');
@@ -60,3 +97,5 @@ changeUsernameBtn.addEventListener('click', () => {
 
 renderUserState();
 applyTheme(startDark); //run theme function on load
+testConnection();
+fetchData();
